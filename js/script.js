119ecf5cca4b0943793f81fcecbf8a37e3e53b80
Kiara Correa Acosta <kcorrea@aclanyc.com>
2018-02-07 02:48:03 +0000

    working product

--- js/script.js
+++ js/script.js
@@ -5,21 +5,66 @@
 // CREATE THE FUNCTIONS BELOW
 
 // Document Ready Function. All of your jQuery should go in here. 
+
+/*global $*/
+// Document Ready Function. All of your jQuery should go in here. 
 $(document).ready(function() {
-    function Vowels(word){
-    return word + "ay";  
+		
+	function setenceToPigLatin(){
+		// var vowels = ["a", "e", "i", "o", "u"]
+		var sentence = $("#userInput").val();
+		sentence = sentence.toString().toLowerCase();
+		var wordsArray = sentence.split(" ");
+		for(var i = 0; i < wordsArray.length; i++){
+			var char = wordsArray[i][0];
+			var secondChar = wordsArray[i][1];
+			var pigLatinized;
+			if(char==="a" || char === "e" || char === "i" || char === "o" || char === "u"){
+				pigLatinized = wordsArray[i] + "yay";
+			}
+			else if(char==="s" && secondChar==="h"){
+				var finalWord =wordsArray[i].substr(2);
+				pigLatinized = finalWord+char+secondChar+"ay";
+			}
+			else{
+				finalWord =wordsArray[i].substr(1);
+				pigLatinized = finalWord+char+"ay";
+			}
+			$("#result").append(pigLatinized + " ");
+		}
+	}
+
+
+
+// Create the wordToPigLatin function that takes a word as a parameter and returns a transfromed word. 
+
+$("#userInput").keyup(function(event) {
+        if (event.keyCode === 13) {
+            $("#translate").click();
+        }
+    });
+	
+	$("#translate").click(function(){
+		$("#result").html("");
+		setenceToPigLatin();
+	});
+});
+
+// $(document).ready(function() {
+//     function Vowels(word){
+//     return word + "ay";  
  
-    }
+//     }
     
-    $("button").click(function(){
-    var message = $("input").val();
-    var PLmessage = Vowels(message);
-    $("#result").text(PLmessage);
-});
+//     $("button").click(function(){
+//     var message = $("input").val();
+//     var PLmessage = Vowels(message);
+//     $("#result").text(PLmessage);
+// });
 
-});
+// });
 
-var splitter = PLmessage.split(' ');
+// var splitter = PLmessage.split(' ');
   
 
 
